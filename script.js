879becbe1fa5d320409099c9942ee9a9928c81f7
jayeshveyro <jayesh.veyro@gmail.com>
2026-09-06 20:13:19 +0530

    Change Supabase key in script.js
    Updated Supabase key to a new value.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("VEYRO SCRIPT LOADED");
 const SUPABASE_URL = "https://ptpnuquothmusvoquhdg.supabase.co";
-const SUPABASE_KEY = "sb_publishable__OCrly2Tq5dKZji3bLtvSw_w0dU2SUq";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0cG51cXVvdGhtdXN2b3F1aGRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNjExNzYsImV4cCI6MjEwMzkzNzE3Nn0.FosTex7ZwEKAxargTx64HVy8KfJftnFhjDX5Yi8ANW4";
 
 const form = document.querySelector("#form");
 const successMessage = document.querySelector("#success");
