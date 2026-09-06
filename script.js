1a18f039449ed401f4d9233cb276492d709b79fe
jayeshveyro <jayesh.veyro@gmail.com>
2026-09-06 19:30:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.querySelectorAll('.nav nav a').forEach((a) => {
 });
 
 // Form submission
-const SUPABASE_URL ="https://ptpnuquothmusvoquhdg.supabase.co/";
+const SUPABASE_URL ="https://ptpnuquothmusvoquhdg.supabase.co";
 const SUPABASE_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0cG51cXVvdGhtdXN2b3F1aGRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNjExNzYsImV4cCI6MjEwMzkzNzE3Nn0.FosTex7ZwEKAxargTx64HVy8KfJftnFhjDX5Yi8ANW4";
 
 const form = document.querySelector("form");
